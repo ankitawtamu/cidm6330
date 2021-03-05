2a8bc7519bdb0fdb05705a5d7709871103f9b416
Ankita Singh <ankita80486@gmail.com>
2021-03-04 18:07:37 -0600

    updated

--- Barky/test_barky_anotherWay.py
+++ Barky/test_barky_anotherWay.py
@@ -9,7 +9,7 @@ class TestCode(unittest.TestCase):
         #actualoutput
         actOutput= {"(A) Add a bookmark (B) List bookmarks by date (T) List bookmarks by title (E) Edit a bookmark (D) Delete a bookmark (G) Import GitHub stars (Q) Quit"}
         #expectedoutput
-        expOutput=print_options()
+        expOutput=Option.print_options()
         self.assertEqual(actOutput,expOutput)
 
 if __name__ == '__main__':
---
+++ Testing/Kata/test_int_to_roman.py
@@ -0,0 +1,58 @@
+
+import unittest
+from Integers_to_Roman import IntegerstoRoman
+
+class Values(unittest.TestCase):
+    values = ( (1, 'I'),
+                     (2, 'II'),
+                     (3, 'III'),
+                     (4, 'IV'),
+                     (5, 'V'),
+                     (6, 'VI'),
+                     (7, 'VII'),
+                     (8, 'VIII'),
+                     (9, 'IX'),
+                     (10, 'X'),
+                     (50, 'L'),
+                     (100, 'C'),
+                     (500, 'D'),
+                     (1000, 'M'),
+                     (31, 'XXXI'),
+                     (148, 'CXLVIII'),
+                     (294, 'CCXCIV'),
+                     (312, 'CCCXII'),
+                     (421, 'CDXXI'),
+                     (528, 'DXXVIII'),
+                     (621, 'DCXXI'),
+                     (782, 'DCCLXXXII'),
+                     (870, 'DCCCLXX'),
+                     (941, 'CMXLI'),
+                     (1043, 'MXLIII'),
+                     (1110, 'MCX'),
+                     (1226, 'MCCXXVI'),
+                     (1301, 'MCCCI'),
+                     (1485, 'MCDLXXXV'),
+                     (1509, 'MDIX'),
+                     (1607, 'MDCVII'),
+                     (1754, 'MDCCLIV'),
+                     (1832, 'MDCCCXXXII'),
+                     (1993, 'MCMXCIII'),
+                     (2074, 'MMLXXIV'),
+                     (2152, 'MMCLII'),
+                     (2212, 'MMCCXII'),
+                     (2343, 'MMCCCXLIII'),
+                     (2499, 'MMCDXCIX'),
+                     (2574, 'MMDLXXIV'),
+                     (2646, 'MMDCXLVI'),
+                     (2723, 'MMDCCXXIII'),
+                     (2892, 'MMDCCCXCII'),
+                     (2975, 'MMCMLXXV'))
+
+    def test_to_roman_values(self):
+        '''to_roman should give known result with known input'''
+        for integer, numeral in self.values:
+            result = IntergerstoRoman.int_to_roman(integer)
+            self.assertEqual(numeral, result)
+
+if __name__ == '__main__':
+    unittest.main()
